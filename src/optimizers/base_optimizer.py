85962cae978bed164ef34e1ab152424abbdb9c3e
Akinsanya Joel <akinsanyajoel82@gmail.com>
2025-06-26 01:47:11 +0100

    setup  base classes

---
+++ src/optimizers/base_optimizer.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class BaseOptimizer(ABC):
+  def __init__(self, lr: float):
+    """
+    Initializes the optimizer.
+
+    Args:
+    lr(float): The learning rate.
+    """
+    if lr <= 0:
+      raise ValueError("Learning rate must be positive.")
+    self.lr = lr
+    self.name = self.__class__.__name__
+
+    @abstractmethod
+    def step(self, params: np.ndarray, gradients: np.ndarray) -> np.ndarray:
+      """
+      Performs a single optimization step.
+
+      Args:
+      params (np.ndarray): Current parameters.
+      gradients (np.ndarray) : Gradients of the loss with respect to
+      the parameters.
+
+      Returns:
+      np.ndarray : Updated parameters.
+      """
+      pass
